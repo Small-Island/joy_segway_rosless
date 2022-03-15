96969c85f96ca3659ccd6c75e709c66f8a321e42
OJIMA-YUKIYA <ojima.yukiya@gmail.com>
2022-03-15 17:03:23 +0900

    update

--- joystick_segway.sh
+++ joystick_segway.sh
@@ -4,5 +4,5 @@ cd ~/joy_segway_rosless
 #/usr/bin/chromium-browser http://127.0.0.1:8080/html/test.html &
 ./momo_arm64.run
 ./test_segway_rmp
-# ~/ojima-librealsense/build/examples/pointcloud/rs-pointcloud
+~/ojima-librealsense/build/examples/pointcloud/rs-pointcloud
 # python3 ./src/mywork/obstacleDetection.py &
