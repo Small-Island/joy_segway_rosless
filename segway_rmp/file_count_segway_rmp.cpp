1894ee8716ee36e36f25e734d7ca8d0ee0fd917c
OJIMA-YUKIYA <ojima.yukiya@gmail.com>
2022-03-22 12:45:29 +0900

    update

--- segway_rmp/file_count_segway_rmp.cpp
+++ segway_rmp/file_count_segway_rmp.cpp
@@ -425,18 +425,18 @@ public:
                         break;
                 }
 
-                // printf("count %d\n", count);
-                // std::cout << "axis:";
-                // for (size_t i(0); i < joy_axis.size(); i++) {
-                //     std::cout << " " << std::setw(2) << joy_axis.at(i)/32767.0;
-                // }
-                // std::cout << '\n';
-                //
-                // std::cout << "  button: ";
-                // for(size_t i(0);i<joy_button.size();++i) {
-                //     std::cout << " " << (int)joy_button.at(i);
-                // }
-                // std::cout << '\n';
+                printf("count %d\n", count);
+                std::cout << "axis:";
+                for (size_t i(0); i < joy_axis.size(); i++) {
+                    std::cout << " " << std::setw(2) << joy_axis.at(i)/32767.0;
+                }
+                std::cout << '\n';
+
+                std::cout << "  button: ";
+                for(size_t i(0);i<joy_button.size();++i) {
+                    std::cout << " " << (int)joy_button.at(i);
+                }
+                std::cout << '\n';
 
 
                 if ((int)joy_button.at(10) && (int)joy_button.at(13)) { //L1 and ○
