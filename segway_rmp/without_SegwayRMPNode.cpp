37d383efa03005f2200b31d52db30bc9fd490864
OJIMA-YUKIYA <ojima.yukiya@gmail.com>
2022-03-24 23:55:03 +0900

    update

--- segway_rmp/without_SegwayRMPNode.cpp
+++ segway_rmp/without_SegwayRMPNode.cpp
@@ -355,15 +355,15 @@ void joy_read() {
                 // my_queue.enqueue(-1.5*joy_axis.at(3)/32767.0 * fabs(joy_axis.at(3)/32767.0));
                 // this->lin = my_queue.mean();
 
-                double A = 1; // 指令値 (m/s) の最大値
+                double A = 1.0; // 指令値 (m/s) の最大値
                 double k = 0.1;
                 double x = -joy_axis.at(3)/32767.0; // joystick の入力値 -1 ~ 1
-                
+
                 if (x > 0) {
-                    joy_lin = A * ((1 - k)*x + k) * x;  // joy_lin は指令値 (m/s)
+                    joy_lin = A*((1 - k)*x + k)*x;  // joy_lin は指令値 (m/s)
                 }
                 else {
-                    joy_lin = - A * ((1 - k)*x + k) * x; // joy_lin は指令値 (m/s)
+                    joy_lin = - A*((1 - k)*x + k)*x; // joy_lin は指令値 (m/s)
                 }
             }
         }
