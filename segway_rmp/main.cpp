c12ca00682ce1a2614a80f19984eda08f81f48b7
OJIMA-YUKIYA <ojima.yukiya@gmail.com>
2022-07-22 12:12:15 +0900

    update

--- segway_rmp/main.cpp
+++ segway_rmp/main.cpp
@@ -926,7 +926,6 @@ int main(int argc, char **argv) {
                         if ((latch != 4 && lin > 0) || (latch == 4 && section != 0)) {
                             if (!emergency_brake) {
                                 emergency_brake = true;
-                                latch = 3;
                                 if (linear_vel_feedback > 0.4) {
                                     emergency_brake_lin = 0.4;
                                 }
