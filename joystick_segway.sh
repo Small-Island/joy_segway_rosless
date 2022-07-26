0b4923ba2204727f459d8d00d84baae63ebeb9de
OJIMA-YUKIYA <ojima.yukiya@gmail.com>
2022-07-26 14:12:56 +0900

    update

--- joystick_segway.sh
+++ joystick_segway.sh
@@ -6,7 +6,8 @@ cd ~/joy_segway_rosless
 if [ $(arch) = 'aarch64' ]; then # jetson xavier NX
     ./momo.run
     ~/ojima-librealsense/build/examples/pointcloud/ojima-pointcloud &
-    ./main
+    # ./main
+    ./delay
 elif [ $(arch) = 'armv7l' ]; then # Raspberry pi
     ./momo.run
     ./main_ras
